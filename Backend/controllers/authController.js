270612be5c8976741bf65fc4722b984d12665240
Snehanjali080 <ramavathsnehanjalibai3@gmail.com>
2026-06-13 12:00:10 +0530

    Fix loginUser

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -46,7 +46,7 @@ export const loginUser = async(req,res)=>{
         const {email,password}=req.body;
 
         //check user
-        const user =await User.findOne({email});
+        const user =await User.findOne({email}).select("+password");
 
         if(!user){
             return res.status(400).json({
